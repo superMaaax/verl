335dfe5c4d30f6b4c5dc3f129803c8bc75820c7d
ShuoZheLi <lishuozhe1999@163.com>
2026-07-04 14:39:24 -0500

    modified:   exp_track/07_04_2026/sft_wanda_sparse_d5_q3_8b_multi_node.sh

--- exp_track/07_04_2026/sft_wanda_sparse_d5_q3_8b_multi_node.sh
+++ exp_track/07_04_2026/sft_wanda_sparse_d5_q3_8b_multi_node.sh
@@ -79,9 +79,9 @@ mkdir -p "$LOG_DIR" "$TRAIN_LOG_DIR" "$ARCHIVE_ROOT"
 # -----------------------------
 # This is a small-data sparse fine-tune. Defaults are conservative for Qwen3-8B.
 train_batch_size=${train_batch_size:-8}
-micro_batch_size_per_gpu=${micro_batch_size_per_gpu:-16}
+micro_batch_size_per_gpu=${micro_batch_size_per_gpu:-1}
 max_length=${max_length:-18432}
-max_token_len_per_gpu=${max_token_len_per_gpu:-294912}
+max_token_len_per_gpu=${max_token_len_per_gpu:-18432}
 lr=${lr:-5e-6}
 total_epochs=${total_epochs:-5}
 save_freq=${save_freq:-50}
@@ -141,7 +141,7 @@ generation_eval_files=${generation_eval_files:-${VAL_FILE}}
 # Generation accuracy eval can be memory-heavy; start small unless you know memory is safe.
 # Qwen3 thinking mode for generation eval. Set generation_eval_enable_thinking=False to disable.
 generation_eval_enable_thinking=${generation_eval_enable_thinking:-True}
-generation_eval_batch_size=${generation_eval_batch_size:-64}
+generation_eval_batch_size=${generation_eval_batch_size:-16}
 generation_max_new_tokens=${generation_max_new_tokens:-18432}
 generation_do_sample=${generation_do_sample:-False}
 generation_temperature=${generation_temperature:-0.0}
@@ -151,10 +151,10 @@ generation_num_samples=${generation_num_samples:-1}
 generation_dtype=${generation_dtype:-null}
 
 generation_backend=${generation_backend:-vllm}
-generation_vllm_gpu_memory_utilization=${generation_vllm_gpu_memory_utilization:-0.7}
+generation_vllm_gpu_memory_utilization=${generation_vllm_gpu_memory_utilization:-0.4}
 generation_vllm_host_ip=${generation_vllm_host_ip:-127.0.0.1}
 generation_vllm_enforce_eager=${generation_vllm_enforce_eager:-True}
-generation_vllm_sync_weights=${generation_vllm_sync_weights:-False}
+generation_vllm_sync_weights=${generation_vllm_sync_weights:-True}
 generation_vllm_enable_multiprocessing=${generation_vllm_enable_multiprocessing:-False}
 
 # -----------------------------
