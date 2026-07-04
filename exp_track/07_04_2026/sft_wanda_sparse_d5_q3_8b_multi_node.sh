04d643349859629a519b5c5f98b4a7b1bb881a81
ShuoZheLi <lishuozhe1999@163.com>
2026-07-04 04:31:24 -0500

    modified:   exp_track/07_04_2026/sft_wanda_sparse_d5_q3_8b_multi_node.sh

--- exp_track/07_04_2026/sft_wanda_sparse_d5_q3_8b_multi_node.sh
+++ exp_track/07_04_2026/sft_wanda_sparse_d5_q3_8b_multi_node.sh
@@ -154,7 +154,7 @@ generation_backend=${generation_backend:-vllm}
 generation_vllm_gpu_memory_utilization=${generation_vllm_gpu_memory_utilization:-0.6}
 generation_vllm_host_ip=${generation_vllm_host_ip:-127.0.0.1}
 generation_vllm_enforce_eager=${generation_vllm_enforce_eager:-True}
-generation_vllm_sync_weights=${generation_vllm_sync_weights:-True}
+generation_vllm_sync_weights=${generation_vllm_sync_weights:-False}
 generation_vllm_enable_multiprocessing=${generation_vllm_enable_multiprocessing:-False}
 
 # -----------------------------
